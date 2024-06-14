0d085504abe3bcc714a0fdcec05ce46077f693ab
Liedson Franco <126687225+LiedsonFranco@users.noreply.github.com>
2024-06-14 13:07:51 +0100

    Mobile and desktop-top design completed

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+      colors: {
+        "Strong-cyan": "hsl(172, 67%, 45%)",
+        "Very-dark-cyan": "hsl(183, 100%, 15%)",
+        "Dark-grayish-cyan": "hsl(186, 14%, 43%)",
+        "Grayish-cyan": "hsl(184, 14%, 56%)",
+        "Light-grayish-cyan": "hsl(185, 41%, 84%)",
+        "Very-light-grayish-cyan": "hsl(189, 41%, 97%)",
+        "white": "hsl(0, 0%, 100%)",
+        "transparent": " hsl(183deg 100% 15% / 0%)"
+      }
+  },
+  plugins: [],
+}
+
